5e28da3f091970d8bd5b6e1f47751d055e238fe4
Vini Neves <60191652+vini-neves@users.noreply.github.com>
2025-12-29 20:41:03 -0300

    Update database configuration keys in settings.py

--- config/settings.py
+++ config/settings.py
@@ -112,9 +112,8 @@ DATABASES = {
         'NAME': config('DATABASE_NAME'),
         'USER': config('DATABASE_USER'),
         'PASSWORD': config('DATABASE_PASSWORD'), 
-        
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
