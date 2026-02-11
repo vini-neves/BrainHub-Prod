fdbaabddf85ec4900baae8f7716eac8b59457a01
vini-neves <vini.leo.neves@gmail.com>
2026-02-11 05:08:51 -0300

    Atualizando a configuração do backend de e-mail para usar 'smtppro.zoho.com' em vez de 'smtp.zoho.com', garantindo uma conexão mais adequada para o envio de e-mails no ambiente de desenvolvimento.

--- config/settings.py
+++ config/settings.py
@@ -223,7 +223,7 @@ GOOGLE_OAUTH_SCOPES = [
 
 # Para desenvolvimento, imprime e-mails no console em vez de enviá-los.
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST = 'smtppro.zoho.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 PASSWORD_RESET_TIMEOUT = 86400
