ce817c34b99fa34060e3d208b31fc4e4d0ea71e2
vini-neves <vini.leo.neves@gmail.com>
2026-02-11 05:31:09 -0300

    Atualizando a configuração do backend de e-mail para usar 'smtppro.zoho.com' com SSL, desativando o uso de TLS e definindo o e-mail padrão a partir do usuário configurado. Isso melhora a segurança e a funcionalidade no ambiente de desenvolvimento.

--- config/settings.py
+++ config/settings.py
@@ -223,13 +223,15 @@ GOOGLE_OAUTH_SCOPES = [
 
 # Para desenvolvimento, imprime e-mails no console em vez de enviá-los.
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_HOST = 'smtppro.zoho.com'
 EMAIL_PORT = 465
-EMAIL_USE_TLS = True
+EMAIL_USE_SSL = True
+EMAIL_USE_TLS = False
 PASSWORD_RESET_TIMEOUT = 86400
 # Lendo do arquivo .env
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 
 
