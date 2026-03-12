746fe83a9bb724e1b488bab356c4a26253af3518
vini-neves <vini.leo.neves@gmail.com>
2026-03-12 11:35:42 -0300

    Atualizando as rotas de redefinição de senha para utilizar templates de registro padrão, melhorando a consistência e a organização dos arquivos de template. As alterações incluem a atualização dos caminhos para os templates de redefinição de senha e confirmação, garantindo uma melhor estruturação do projeto.

--- projects/urls.py
+++ projects/urls.py
@@ -19,18 +19,21 @@ urlpatterns = [
 
    # --- ESQUECI A SENHA ---
     path('password_reset/', auth_views.PasswordResetView.as_view(
-        template_name='projects/password_reset_form.html',
-        email_template_name='projects/password_reset_email.html',
-        subject_template_name='projects/password_reset_subject.txt'
+        template_name='registration/password_reset_form.html',
+        email_template_name='registration/password_reset_email.html',
+        subject_template_name='registration/password_reset_subject.txt'
     ), name='password_reset'),
+    
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
-        template_name='projects/password_reset_done.html'
+        template_name='registration/password_reset_done.html'
     ), name='password_reset_done'),
+    
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-        template_name='projects/password_reset_confirm.html'
+        template_name='registration/password_reset_confirm.html'
     ), name='password_reset_confirm'),
+    
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
-        template_name='projects/password_reset_complete.html'
+        template_name='registration/password_reset_complete.html'
     ), name='password_reset_complete'),
 
     # --- KANBAN UNIFICADO (GERAL & OPERACIONAL) ---
