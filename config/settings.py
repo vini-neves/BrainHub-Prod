67c6a9fea552291b4206747415de7db00fecda16
vini-neves <vini.leo.neves@gmail.com>
2026-03-12 14:44:08 -0300

    Atualizando o código do idioma para 'pt-br', alterando a configuração de internacionalização no arquivo de configurações.

--- config/settings.py
+++ config/settings.py
@@ -173,7 +173,7 @@ ROOT_URLCONF = 'config.urls'
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
